2a0c352305e02159f7c204354feb43b4e9993897
dksgudwn <dksgudwn620@gmail.com>
2023-04-10 18:26:05 +0900

    1356312312323123

--- 2gang/Bombman/console.cpp
+++ 2gang/Bombman/console.cpp
@@ -2,4 +2,46 @@
 
 void FullScreen()
 {
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleDisplayMode(hOut, CONSOLE_FULLSCREEN_MODE, NULL);
+}
+
+void Gotoxy(int x, int y)
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD Cur = { x,y };
+	SetConsoleCursorPosition(hOut, Cur);
+}
+
+void Cursorset(bool _bVis, DWORD _dwSize)
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO Curinfo;
+	Curinfo.bVisible = _bVis;
+	Curinfo.dwSize = _dwSize;
+	SetConsoleCursorInfo(hOut, &Curinfo);
+}
+
+void SetColor(int _color, int _bgColor)
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hOut, (_bgColor << 4) | _color);
+}
+
+int GetColor()
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(hOut, &info);
+	int color = info.wAttributes & 0xf;
+	return color;
+}
+
+int GetbgColor()
+{
+	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO info;
+	GetConsoleScreenBufferInfo(hOut, &info);
+	int color = (info.wAttributes & 0xf) >> 4;
+	return color;
 }
